4e0f7680eb733d5209976807358a8b4fdf989f9e
ailendr <57049339+ailendr@users.noreply.github.com>
2022-08-10 11:01:40 -0300

    Agrego unas cosas

---
+++ src/main/java/domain/garantias/Garantia.java
@@ -0,0 +1,11 @@
+package domain.garantias;
+
+import java.util.Date;
+
+public class Garantia {
+  private Date fechaDeInicio;
+  private int duracion;
+  private Float precio;
+  private Boolean vigente;
+
+}
